138bfa848c2cd049ce3e94fd5e776b4e236bf59e
Jens Kouros <jens.kouros@posteo.de>
2020-02-10 12:44:27 +0100

    Make Main.hs a module

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,21 @@
+module Main where
+
+import Data.Char
+
+checkPasswordLength :: String -> Maybe String
 checkPasswordLength password =
   case length password > 20 of
     True -> Nothing
-    False -> Just passwordsword > 20 of
-      True -> Nothing
-      False -> Just password
+    False -> Just password
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum password =
+  case all isAlphaNum password of
+    False -> Nothing
+    True -> Just password
 
 main :: IO ()
 main = do
   putStr "Enter Password\n"
   password <- getLine
-  print (checkPasswordLength password)
+  print (requireAlphaNum password)
