f5b7cb20132fd98759cc787a0508816afbcd2be2
Jens Kouros <jens.kouros@posteo.de>
2020-02-17 12:28:14 +0100

    Implement tests (Exercise 15)

--- src/Main.hs
+++ src/Main.hs
@@ -52,3 +52,23 @@ main = do
   putStr "Enter Password\n"
   password <- getLine
   print (validatePassword password)
+
+printTestResult :: Either String () -> IO ()
+printTestResult r =
+  case r of
+    Left err -> putStrLn err
+    Right () -> putStrLn "All tests passed."
+
+eq :: (Eq a, Show a) => Int -> a -> a -> Either String ()
+eq n actual expected =
+  case (actual == expected) of
+    True -> Right ()
+    False -> Left (unlines
+      ["Test " ++ show n , " Expected: " ++ show expected, " But got: " ++ show actual])
+
+test :: IO ()
+test = printTestResult $
+  do
+    eq 1 (checkPasswordLength "") (Left "Your password must be between 10 and 20 characters long")
+    eq 2 (checkPasswordLength "julielovesbooks") (Right "julielovesbooks")
+    eq 3 (validatePassword "1234567890") (Left "Your password must be between 10 and 20 characters long")
